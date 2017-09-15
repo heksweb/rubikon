355a88741047b327c2fd0b10f584d0eead6c66b4
root <evdokim1988@inbox.ru>
2017-09-15 23:06:53 +0300

    first commit

---
+++ app/site/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysqldb;dbname=bizzone',
+    'username' => 'root',
+    'password' => 'am3512673',
+    'charset' => 'utf8',
+];
